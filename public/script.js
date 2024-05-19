1eff90a1416b3cd8a7b7cd309224a5f54ac78b00
Angelos Kalofas <kaloeake@icloud.com>
2024-05-19 12:47:37 +0300

    Final Simple ChatBot without database.

--- public/script.js
+++ public/script.js
@@ -1,14 +1,69 @@
-// Connect to server
-// const socket = io.connect('http://localhost:5850');
+// Socket Connection Handlers
+// --------------------------
+// Handle initial connection, disconnections, and connection errors.
+socket.on('initial-connection', (messages) => {
+	chatlistEl.innerHTML = '';
+	messages.forEach(appendMessage);
+});
 
-// DOM elements
+socket.on('connect_error', (error) => {
+	console.error('Connection failed:', error);
+});
+
+socket.on('disconnect', () => {
+	console.log('Disconnected from server');
+});
+
+// DOM Elements
+// ------------
+// References to the DOM elements used in the script.
 const messageEl = document.getElementById('message');
 const handleEl = document.getElementById('handle');
 const btnEl = document.getElementById('send');
 const chatlistEl = document.getElementById('chatlist');
 const feedbackEl = document.getElementById('feedback');
 
+// Event Listeners
+// ---------------
+// Setup event listeners for sending messages and displaying typing feedback.
+btnEl.addEventListener('click', sendMessage);
+messageEl.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        sendMessage(event);
+    }
+});
+
+messageEl.addEventListener('keypress', () => {
+    socket.emit('typing', handleEl.value);
+});
+
+// Socket Event Handlers
+// ---------------------
+// Listen for chat messages and typing events.
+socket.on('chat', appendMessage);
+
+socket.on('typing', (data) => {
+    clearTimeout(typingTimeout);
+    feedbackEl.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
+    typingTimeout = setTimeout(() => (feedbackEl.innerHTML = ''), 2000);
+});
+
+// Shared Variables
+// ------------------
+// Typing indicator
+let typingTimeout;
+// Track the last sender whose name was displayed
+let lastDisplayedSender = null; 
+
 // Shared Functions
+// -----------------
+// Auto Scroll to bottom when send or receive a message
+function scrollToBottom() {
+	chatlistEl.scrollTop = chatlistEl.scrollHeight;
+}
+
+
+// Function for sending messages
 function sendMessage(event) {
     if (messageEl.value.trim() && handleEl.value.trim()) {
         socket.emit('chat', {
@@ -19,58 +74,36 @@ function sendMessage(event) {
     }
 }
 
-// Emit chat message event
-btnEl.addEventListener('click', sendMessage);
-messageEl.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        sendMessage(event);
-    }
-})
-
-// Emit typing event
-messageEl.addEventListener('keypress', () => {
-	socket.emit('typing', handleEl.value);
-});
-
-// Listen for initial connection and load chat history
-socket.on('initial-connection', (messages) => {
-	chatlistEl.innerHTML = '';
-	messages.forEach(appendMessage);
-});
-
-// Listen for chat messages and append them
-socket.on('chat', appendMessage);
-
-// Display typing feedback
-let typingTimeout;
-socket.on('typing', (data) => {
-	clearTimeout(typingTimeout);
-	feedbackEl.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
-	typingTimeout = setTimeout(() => (feedbackEl.innerHTML = ''), 32000);
-});
-
-// Function to append messages to chat
+// Function for appending messages to the chat list.
 function appendMessage(msg) {
     clearTimeout(typingTimeout);
-	const messageDiv = document.createElement('div');
-    messageDiv.setAttribute('data-time', msg.handle);
-    messageDiv.classList.add('msg');
-    messageDiv.textContent = `${msg.message}`;
+
+    const messageDiv = document.createElement('div');
+    const messageTextDiv = document.createElement('div');
+    const senderDiv = document.createElement('div');
+    messageTextDiv.classList.add('chat__bubble');
+    messageTextDiv.textContent = msg.message;
+
     if (msg.handle === handleEl.value) {
-        messageDiv.classList.add('send');
-	    chatlistEl.appendChild(messageDiv);
+        messageDiv.classList.add('chat__bubble--sent');
+        messageTextDiv.classList.add('chat__message--sent');
+        senderDiv.classList.add('chat__message--sender');
     } else {
-        messageDiv.classList.add('rcvd');
-        chatlistEl.appendChild(messageDiv);
+        messageDiv.classList.add('chat__bubble--received');
+        messageTextDiv.classList.add('chat__message--received');
+        senderDiv.classList.add('chat__message--receiver');
     }
+
+    let showSender = lastDisplayedSender !== msg.handle;
+
+    if (showSender) {
+        senderDiv.textContent = msg.handle;
+        messageDiv.appendChild(senderDiv);
+        lastDisplayedSender = msg.handle;
+    }
+
+    messageDiv.appendChild(messageTextDiv);
+
+    chatlistEl.appendChild(messageDiv);
+    scrollToBottom();
 }
-
-// Handle connection errors
-socket.on('connect_error', (error) => {
-	console.error('Connection failed:', error);
-});
-
-// Handle disconnection
-socket.on('disconnect', () => {
-	console.log('Disconnected from server');
-});
